cdf97a7a0088688a45e76578c4be277da58b33c0
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2021-12-29 16:59:12 +0330

    ok

--- js.js
+++ js.js
@@ -1,13 +1,5 @@
-function finalGrade(exam, projects) {
-  if (exam > 90 || projects > 1) {
-    console.log("100 A+");
-  } else if (exam > 75 && 0 > projects < 5) {
-    console.log("90 A");
-  } else if (exam > 50 && 0 > projects < 2) {
-    console.log("75 B-");
-  } else {
-    console.log("0");
-  }
-}
+function shortcut(string) {
 
-finalGrade(85, 5);
+  console.log(string.replace( /[aeiou]/g, '' ));
+}
+shortcut("hEllO");
