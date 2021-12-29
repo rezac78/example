1aef528a32581a108cb9c88f55e85ca72c14e36c
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2021-12-29 16:49:10 +0330

    ok

--- js.js
+++ js.js
@@ -1,10 +1,13 @@
-function squareSum(numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i] ** 2;
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 1) {
+    console.log("100 A+");
+  } else if (exam > 75 && 0 > projects < 5) {
+    console.log("90 A");
+  } else if (exam > 50 && 0 > projects < 2) {
+    console.log("75 B-");
+  } else {
+    console.log("0");
   }
-  // return sum;
-  console.log(sum)
 }
 
-squareSum([1, 2, 2]);
+finalGrade(85, 5);
