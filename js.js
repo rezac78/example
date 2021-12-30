e9c217e7ceb8b2eb615aac3dea803efd6d60eb61
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2021-12-30 15:48:52 +0330

    ok

--- js.js
+++ js.js
@@ -1,5 +1,17 @@
-function shortcut(string) {
-
-  console.log(string.replace( /[aeiou]/g, '' ));
+function bmi(weight, height) {
+  const bmi = (weight) / (height * height)
+  console.log(bmi)
+  // if (bmi <= 18.5) {
+  //   console.log("Underweight");
+  // } else if (bmi <= 25) {
+  //   console.log("Normal");
+  // } else if (bmi <= 30) {
+  //   console.log("Overweight");
+  // } else if (bmi > 30) {
+  //   console.log("Obese");
+  // } else {
+  //   console.log("ooos");
+  // }
 }
-shortcut("hEllO");
+
+bmi(95, 183);
