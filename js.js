1dabb3aac8b30c91640056bc3d8a534a0ab6e5c8
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2021-12-30 17:15:08 +0330

    ok

--- js.js
+++ js.js
@@ -1,35 +1,5 @@
-var array1 = [
-  true,
-  true,
-  true,
-  false,
-  true,
-  true,
-  true,
-  true,
-  true,
-  false,
-  true,
-  false,
-  true,
-  false,
-  false,
-  true,
-  true,
-  true,
-  true,
-  true,
-  false,
-  false,
-  true,
-  true,
-];
-function countSheeps(arrayOfSheep) {
-  let counter = 0
-  for(let i = 0; i < arrayOfSheep.length; i++) {
-      if(arrayOfSheep[i]) counter += 1
-  }
-  console.log(counter);
+function booleanToString(b){
+  console.log(booleanToString(b))
 }
 
-countSheeps(array1);
+booleanToString(01001100)
