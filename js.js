bf3829296b4d4c627e0bac2a642ce47df30e51ed
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-01-02 18:28:06 +0330

    ok

--- js.js
+++ js.js
@@ -1,13 +1,6 @@
-function getCount(str) {
-  var vowelsCount = 0;
-  const vowels = ["a", "e", "i", "o", "u"];
-  for (let char of str) {
-    if(vowels.includes(char)) {
-      vowelsCount++;
-    }
-  }
-
-  return vowelsCount;
+function disemvowel(str) {
+  const reza = str.replace(/[aeiou]/gi,"");
+  console.log(reza)
 }
 
-getCount("abracadabra");
+disemvowel("This website is for losers LOL");
