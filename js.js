2a21b478bfd6594bc1b28b64dedf42f420ceac16
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-01-03 17:37:09 +0330

    ok

--- js.js
+++ js.js
@@ -1,13 +1,14 @@
-function getMiddle(s) {
-  const lengthNum = s.length;
-  if (lengthNum % 2 == 1) {
-    const odd = Math.floor(lengthNum / 2);
-    return s[odd];
-  }else{
-    const even = Math.floor(lengthNum / 2);
-    const even1 = Math.floor((lengthNum / 2)-1);
-    // return ;
-    console.log(s[even]+s[even1])
+function accum(s) {
+  const result = [];
+  const lowerStr = s.toLowerCase();
+  for (let i = 0; i < lowerStr.length; i++) {
+    let str = lowerStr[i].toUpperCase();
+    for (let j = 0; j < i; j++) {
+      str += lowerStr[i];
+    }
+    result.push(str);
   }
+  return result.join("-");
 }
-getMiddle("test");
+
+accum("abcd");
