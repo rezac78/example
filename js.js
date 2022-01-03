9cf6467165dfefc285fed7b70a4ac6c7a6b500f9
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-01-03 17:53:35 +0330

    ok

--- js.js
+++ js.js
@@ -1,6 +1,9 @@
-function findShort(s){
-  const result = Math.min(...s.split(' ').map((word) => word.length));
-  return result;
-}
-
-findShort("turns out random test cases are easier than writing out basic ones");
+String.prototype.toJadenCase = function () {
+  let jCased = [];
+  let words = this.toLowerCase().split(" ");
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    jCased.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return jCased.join(" ");
+};
