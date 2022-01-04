aa5e28bee700dcd6610ae45c5a9fb7189c75d94b
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-01-04 19:57:06 +0330

    ok

--- js.js
+++ js.js
@@ -1,5 +1,13 @@
-function filter_list(l) {
-  console.log(l.filter((x) => typeof x === "number"));
+function isIsogram(str) {
+  const newStr = str.toLowerCase();
+  for (let i = 0; i < newStr.length; i++) {
+    for (let j = i + 1; j < newStr.length; j++) {
+      if (newStr[i] === newStr[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
-filter_list([1, 2, "a", "b"]);
+isIsogram("aba");
