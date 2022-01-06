3e39a9e327019931c6c2361151a5bed9a6ee9057
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-01-06 12:48:22 +0330

    ok

--- js.js
+++ js.js
@@ -1,4 +1,8 @@
-function friend(friends) {
-  return friends.filter((el) => el.length === 4);
+function createPhoneNumber(numbers) {
+  const perNumber = numbers.slice(0, 3).join("");
+  const number = numbers.slice(3, 6).join("");
+  const numberPer = numbers.slice(6, 10).join("");
+  return `(${perNumber}) ${number}-${numberPer}`;
 }
-friend(["Ryan", "Kieran", "Mark"]);
+
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
