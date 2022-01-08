8da4e5ac3d25e06081c9b2f4b702bfd590626271
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-01-08 10:42:25 +0330

    ok

--- js.js
+++ js.js
@@ -1,16 +1,5 @@
-function spinWords(string) {
-  const sentence = "";
-  const separate = string.split(" ");
-
-  for (let i = 0; i < separate.length; i++) {
-    if (sentence) sentence += " ";
-    if (separate[i].length >= 5) {
-      sentence += separate[i].split("").reverse().join("");
-    }else{
-      sentence += separate[i]
-    }
-  }
-  return sentence;
+function arrayDiff(a, b) {
+  return a.filter((e) => !b.includes(e));
 }
 
-spinWords("This is another tes");
+arrayDiff([1, 2, 2, 2, 3], [2]);
