ad34828f942fc02c3b0f4dac7c9db9fdf6cf16aa
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-01-08 11:06:58 +0330

    ok

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
-function arrayDiff(a, b) {
-  return a.filter((e) => !b.includes(e));
+function findOutlier(int) {
+  var even = int.filter((a) => a % 2 === 0);
+  var odd = int.filter((a) => a % 2 !== 0);
+  return even.length === 1 ? even[0] : odd[0];
 }
 
-arrayDiff([1, 2, 2, 2, 3], [2]);
+findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]);
