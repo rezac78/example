a0d447685633beaa39ddd8c4e443498b901aae21
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-01-09 11:25:14 +0330

    ok

--- js.js
+++ js.js
@@ -1,13 +1,9 @@
-function toCamelCase(str) {
-  str = str.split("");
-  return str
-    .map(function (el, i) {
-      if (el == "-" || el == "_") {
-        el = str[i + 1].toUpperCase();
-        str.splice(i + 1, 1);
-      }
-      return el;
-    })
-    .join("");
+function isPangram(string){
+  string = string.toLowerCase();
+  return "abcdefghijklmnopqrstuvwxyz"
+    .split("").every(function(x){
+      return string.indexOf(x) !== -1;
+  });
 }
-toCamelCase("the-stealth-warrio");
+
+isPangram("The quick brown fox jumps over the lazy dog")
