f1cd841f945b116b0b5a7514bd2e1bc4b07d9cf7
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-01-09 10:58:21 +0330

    ok

--- js.js
+++ js.js
@@ -1,4 +1,13 @@
 function toCamelCase(str) {
-  
+  str = str.split("");
+  return str
+    .map(function (el, i) {
+      if (el == "-" || el == "_") {
+        el = str[i + 1].toUpperCase();
+        str.splice(i + 1, 1);
+      }
+      return el;
+    })
+    .join("");
 }
-toCamelCase("the-stealth-warrior");
+toCamelCase("the-stealth-warrio");
