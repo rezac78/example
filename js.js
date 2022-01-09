5305e04aa5eaf07a8f8f5fc5f896f8e54f6755d5
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-01-09 10:54:54 +0330

    ok

--- js.js
+++ js.js
@@ -1,10 +1,4 @@
-var uniqueInOrder = function (iterable) {
-  const newStr = [];
-  for (let i = 0; i < iterable.length; i++) {
-    if (iterable[i] !== iterable[i + 1]) {
-      newStr.push(iterable[i])
-    }
-  }
-  return newStr
-};
-uniqueInOrder("AAAABBBCCDAABBB");
+function toCamelCase(str) {
+  
+}
+toCamelCase("the-stealth-warrior");
