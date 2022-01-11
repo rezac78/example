6c5724849a918b96b347d813ca2f9fee62065796
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-01-11 11:27:12 +0330

    ok

---
+++ js.js
@@ -0,0 +1,14 @@
+function solution(str) {
+  var i = 0;
+  var result = new Array();
+  if (str.length % 2 !== 0) {
+    str = str + "_";
+  }
+  while (i < str.length) {
+    result.push(str[i] + str[i + 1]);
+    i += 2;
+  }
+  return result;
+}
+
+solution("abcdef");
