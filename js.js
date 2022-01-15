b2d497110cc80cd21da6eeb9de92658f872245a0
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-01-15 11:15:46 +0330

    ok

--- js.js
+++ js.js
@@ -1,17 +1,16 @@
-function isPrime(num) {
-  if (num < 2) {
-    return false;
-  }
-  if (num === 2) {
-    return true;
-  }
-
-  const maximumDividor = Math.sqrt(num) + 1;
-  for (let i = 2; i < maximumDividor; i++) {
-    if (num % i === 0) {
-      return false;
+function sortArray(array) {
+  const even = array.map((elm) => (elm % 2 == 0 ? elm : ""));
+  const odd = array.filter((elm) => elm % 2 == 1).sort((a, b) => a - b);
+  const result = [];
+  for (let i = 0, j = 0; i < array.length; i += 1) {
+    if (even[i] === "") {
+      result.push(odd[+j]);
+      j += 1;
+    } else {
+      result.push(even[i]);
     }
   }
-  return true;
+  return result;
 }
-isPrime(1);
+
+sortArray([5, 8, 6, 3, 4]);
